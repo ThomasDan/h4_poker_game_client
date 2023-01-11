7b025032e7df8d095fc51c3d31f2251f76430bc1
Thomas Carl Alexander Dänhardt <thomas.danhardt@gmail.com>
2023-01-11 10:37:31 +0100

    WEbsocket Test 3

--- lib/services/web_socket_service.dart
+++ lib/services/web_socket_service.dart
@@ -6,14 +6,14 @@ import 'package:flutter/material.dart';
 class WebSocketService extends StatefulWidget {
   //final Function newMessageReceived;
 
-  // WebSocketService();//this.newMessageReceived);
+  // WebSocketService(this.newMessageReceived);
 
   @override
-  _WebSocketServiceState createState() => _WebSocketServiceState();
+  State<WebSocketService> createState() => _WebSocketServiceState();
 }
 
 class _WebSocketServiceState extends State<WebSocketService> {
-  // https://us-central1-pokergameengine-firebase.cloudfunctions.net/helloWorld
+  // jacob WS test:  https://us-central1-pokergameengine-firebase.cloudfunctions.net/helloWorld
   var channel = IOWebSocketChannel.connect(Uri.parse('ws://localhost:8080'));
   List<String> messages = [
     'Test 1: This is not from WS',
@@ -24,32 +24,60 @@ class _WebSocketServiceState extends State<WebSocketService> {
     channel.sink.add('Hello?');
     channel.stream.listen((message) {
       channel.sink.add('Received!');
-      channel.sink.close(status.goingAway);
+      //channel.sink.close(status.goingAway);
       setState(() {
         messages.add(message);
       });
     });
   }
 
-  // https://www.flutterbeads.com/call-method-after-build-in-flutter/
-  @override
-  void initState() {
-    // this initState() is called right after the Widget is done building.
-    super.initState();
-    WidgetsBinding.instance?.addPostFrameCallback((_) {
-      listen();
+  // https://www.youtube.com/watch?v=lkpPg0ieklg (streambuilder)
+  /*
+  Stream<String> receiveMessage() async* {
+    channel.stream.listen((message) {
+      channel.sink.add('Received!');
+      setState(() {
+        messages.add(message);
+      });
     });
   }
+  */
+
+  // https://www.flutterbeads.com/call-method-after-build-in-flutter/
+  //@override
+  //void initState() {
+  // this initState() is called right after the Widget is done building.
+  //  super.initState();
+  //  WidgetsBinding.instance?.addPostFrameCallback((_) {
+  //    listen();
+  //  });
+  //}
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
+    listen();
+    channel.sink.add('HELLO?');
+    return Text(messages.join('\n'));
+
+    /*StreamBuilder(
       stream: channel.stream,
       builder: (context, snapshot) {
-        return Text(snapshot.hasData ? '${snapshot.data}' : '');
+        String result;
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          result = 'Waiting for connection..';
+        } else if (snapshot.hasData) {
+          result = snapshot.data;
+        } else if (snapshot.hasError) {
+          result = 'Error: ${snapshot.error.toString()}';
+        } else {
+          result = '???';
+        }
+        return Text(result);
       },
     );
+    */
 
+    /*
     //return ListView.builder(
     //  itemBuilder: (context, index) {
     //    return ListTile(
@@ -62,5 +90,6 @@ class _WebSocketServiceState extends State<WebSocketService> {
     //  },
     //  itemCount: messages.length,
     //);
+    */
   }
 }
